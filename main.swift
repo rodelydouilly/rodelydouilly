3a1b1468793a86c4ccf692646a734da42a4f9558
Rodely Douilly <rodelydouilly@gmail.com>
2024-02-27 00:14:16 -0500

    Update main.swift

--- main.swift
+++ main.swift
@@ -30,12 +30,11 @@ for index in indices {
 
 
 
-
 //3.-Affichez tous les mots de la liste sous la forme Moyen de transport numéro [i] est [mot]
 for index in 0..<tableauMots.count {
     print("Moyen de transport numéro \(index) est: \(tableauMots[index])")
     print()
-   
+
 }
 
 
@@ -60,6 +59,48 @@ for index in 0..<tableauMots.count where index % 2 != 0
     print()
 }
 
+
+//5.Inversez l'ordre du tableau puis stockez le résultat dans un nouveau tableau
+let tableauMots = ["bateau", "voiture", "velo", "avion", "bus", "train", "moto"]
+let nouveauTableau = tableauMots.reversed()
+let nouveauTableauArray = Array(nouveauTableau)
+print(nouveauTableauArray) 
+
+
+
+
+//6.Insérez les mots suivants ‘camion’, ‘taxi’ dans l'entete du tableau
+
+var tableauMots = ["bateau", "voiture", "velo", "avion", "bus", "train", "moto"]
+
+let nouveauxMots = ["camion", "taxi"]
+
+tableauMots.insert(contentsOf: nouveauxMots, at: 0)
+
+print(tableauMots) 
+
+
+
+
+//Insertion a la fin
+var tableauMots = ["bateau", "voiture", "velo", "avion", "bus", "train", "moto"]
+
+let nouveauxMots = ["camin", "taxi"]
+
+tableauMots.append(contentsOf: nouveauxMots)
+
+print(tableauMots) 
+
+
+
+//Insertion  a partir de l'indice 2
+var tableauMots = ["bateau", "voiture", "velo", "avion", "bus", "train", "moto"]
+
+let nouveauxMots = ["camion", "taxi"]
+
+tableauMots.insert(contentsOf: nouveauxMots, at: 2)
+
+print(tableauMots) 
 
 
 
